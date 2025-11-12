3b6cf70d4d20c005e99e4f6c67f54d0cbe4469c7
ghostp8 <>
2025-11-12 00:52:07 +0000

    create list of all backend functions

--- src/backend.js
+++ src/backend.js
@@ -1,22 +1,162 @@
 
 
+const url = "http://dementica.danigoes.online:80/"
+var MyUserID = ""
 
-export function backend_request(str, callback){
-	fetch('http://localhost:80/v1/say_hello', {
+export function backend_register(name,password,email, callback){
+	fetch(url+"v1/register", {
 		method: 'POST',
 		headers: {
 			'Accept': 'application/json',
 			'Content-Type': 'application/json',
 		},
 		body: JSON.stringify({
-			name: str
+			Name: name,
+			Password: password,
+			UserType: "Doctor",
+			RegType: "EMAIL",
+			RegisterWith: email
 		})
 	})
 	.then(response => response.json() )
-	.then(data => { callback(data.message); })
+	.then(data => { callback(data); })
 	.catch((err)=>{ console.log("BACKEND: "+err.message) } )
 }
 
+export function backend_login(name,password, callback){
+	fetch(url+"v1/login", {
+		method: 'POST',
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			Name: name,
+			Password: password,
+			UserType: "Doctor",
+		})
+	})
+	.then(response => response.json() )
+	.then(data => { MyUserID = data.UserID; callback(data); })
+	.catch((err)=>{ console.log("BACKEND: "+err.message) } )
+}
 
+export function backend_logout() {
+	return True }
+
+
+
+export function backend_PatientInfo(userID, callback){
+	fetch(url+"v1/patient_info", {
+		method: 'POST',
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			UserID: UserID
+		})
+	})
+	.then(response => response.json() )
+	.then(data => { callback(data); })
+	.catch((err)=>{ console.log("BACKEND: "+err.message) } )
+}
+export function backend_GetRiskScore(userID, callback){
+	fetch(url+"v1/get_risk", {
+		method: 'POST',
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			UserID: UserID
+		})
+	})
+	.then(response => response.json() )
+	.then(data => { callback(data); })
+	.catch((err)=>{ console.log("BACKEND: "+err.message) } )
+}
+
+export function backend_MyData(callback){
+	fetch(url+"v1/doctor_info", {
+		method: 'POST',
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			UserID: MyUserID
+		})
+	})
+	.then(response => response.json() )
+	.then(data => { callback(data); })
+	.catch((err)=>{ console.log("BACKEND: "+err.message) } )
+}
+
+export function backend_GetPatients(callback){
+	fetch(url+"v1/patients", {
+		method: 'POST',
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			UserID: MyUserID
+		})
+	})
+	.then(response => response.json() )
+	.then(data => { callback(data); })
+	.catch((err)=>{ console.log("BACKEND: "+err.message) } )
+}
+
+export function backend_PatientTestHistory(userId, callback){
+	fetch(url+"v1/test_history", {
+		method: 'POST',
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			UserID: userId
+		})
+	})
+	.then(response => response.json() )
+	.then(data => { callback(data); })
+	.catch((err)=>{ console.log("BACKEND: "+err.message) } )
+}
+
+export function backend_SetDementia(userId, demStr, callback){
+	// demStr must be of type "Unknown", "Positive", "Negative"
+	fetch(url+"v1/send_dementia", {
+		method: 'POST',
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			UserID: userId,
+			Dementia: demStr
+		})
+	})
+	.then(response => response.json() )
+	.then(data => { callback(data); })
+	.catch((err)=>{ console.log("BACKEND: "+err.message) } )
+}
+
+export function backend_GetNews(callback){
+	fetch(url+"v1/get_news", {
+		method: 'POST',
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			Type:"Doctor"
+		})
+	})
+	.then(response => response.json() )
+	.then(data => { callback(data); })
+	.catch((err)=>{ console.log("BACKEND: "+err.message) } )
+}
 
 
