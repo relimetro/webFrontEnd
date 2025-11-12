875fc15edadc41f3e20bb60e95037bd6b477d144
ghostp8 <>
2025-11-12 01:59:32 +0000

    add lazy documentation to backend functions

--- src/backend.js
+++ src/backend.js
@@ -1,8 +1,24 @@
 
 
-const url = "http://dementica.danigoes.online:80/"
-var MyUserID = ""
 
+// their is some basic example code in pages/patients.js
+// note danis code only sometimes responds to requests so until I fix that might need to call multiple times (will get 501 not implemented)
+
+// each function takes in arguments which should be self explanatory, the last argument is always a callback which will be called when response is recieved
+// I have added a list of fields the return object will have above each function
+// I will create more detailed documentation when I am less lazy
+
+
+
+const url = "http://dementica.danigoes.online:80/"
+// const url = "http://localhost:80/"
+
+var MyUserID = "" // internal use / private
+
+
+
+// create a new user
+// return: .Result (Ok, Taken)
 export function backend_register(name,password,email, callback){
 	fetch(url+"v1/register", {
 		method: 'POST',
@@ -23,6 +39,9 @@ export function backend_register(name,password,email, callback){
 	.catch((err)=>{ console.log("BACKEND: "+err.message) } )
 }
 
+// Login to existing user; required for all other functions to work as they assume already logged in
+// return: .UserID .Result (Ok,UserPass (incorrect password or username), Type, given user is a patient not doctor) // note, many of these checks are not actualy implemented in backend yet
+// in future when i integrate danis pos code, this will return a session_token, not userID, but i should be able to change these functions without affecting your code (just dont use UserID)
 export function backend_login(name,password, callback){
 	fetch(url+"v1/login", {
 		method: 'POST',
@@ -41,11 +60,13 @@ export function backend_login(name,password, callback){
 	.catch((err)=>{ console.log("BACKEND: "+err.message) } )
 }
 
+// the function i put most effort into
 export function backend_logout() {
-	return True }
+	return true }
 
 
-
+// returns patient data based on UserID (see getPatients for getting patient userIds)
+// return: Result(Ok,NotFound,NoPermission,NotPatient) Name, HasDementia(Unknown,Positive,Negative), DoctorID (your id), RiskScore ("" or float 0.0--1.0)
 export function backend_PatientInfo(userID, callback){
 	fetch(url+"v1/patient_info", {
 		method: 'POST',
@@ -54,13 +75,14 @@ export function backend_PatientInfo(userID, callback){
 			'Content-Type': 'application/json',
 		},
 		body: JSON.stringify({
-			UserID: UserID
+			UserID: userID
 		})
 	})
 	.then(response => response.json() )
 	.then(data => { callback(data); })
 	.catch((err)=>{ console.log("BACKEND: "+err.message) } )
 }
+// simular to above but only .Result and .RiskScore
 export function backend_GetRiskScore(userID, callback){
 	fetch(url+"v1/get_risk", {
 		method: 'POST',
@@ -69,7 +91,7 @@ export function backend_GetRiskScore(userID, callback){
 			'Content-Type': 'application/json',
 		},
 		body: JSON.stringify({
-			UserID: UserID
+			UserID: userID
 		})
 	})
 	.then(response => response.json() )
@@ -77,6 +99,8 @@ export function backend_GetRiskScore(userID, callback){
 	.catch((err)=>{ console.log("BACKEND: "+err.message) } )
 }
 
+// get your information
+// returns: .Result(Ok,NotFound,NoPermission,NotDoctor), Name, Email
 export function backend_MyData(callback){
 	fetch(url+"v1/doctor_info", {
 		method: 'POST',
@@ -93,6 +117,8 @@ export function backend_MyData(callback){
 	.catch((err)=>{ console.log("BACKEND: "+err.message) } )
 }
 
+// all patients assigned to loged in doctor
+// returns .Result(Ok,NotFound,NoPermission,NotDoctor) .Patients (list of patients with same info as PatientInfo)
 export function backend_GetPatients(callback){
 	fetch(url+"v1/patients", {
 		method: 'POST',
@@ -109,6 +135,9 @@ export function backend_GetPatients(callback){
 	.catch((err)=>{ console.log("BACKEND: "+err.message) } )
 }
 
+// returns list of all patient tests and calculated risk score (rather than just overall risk score)
+// returns .Result (Ok,NotFound,NoPermission,NotPatient), .Tests (list of...
+	// .Date, .RiskScore ("Calculating" or a float)
 export function backend_PatientTestHistory(userId, callback){
 	fetch(url+"v1/test_history", {
 		method: 'POST',
@@ -125,6 +154,8 @@ export function backend_PatientTestHistory(userId, callback){
 	.catch((err)=>{ console.log("BACKEND: "+err.message) } )
 }
 
+// set a patients dementia status (Unknown,Positive,Negative)
+// returns .Result (Ok,NotFound,NoOperation (misspelling of no permission), NotPatient)
 export function backend_SetDementia(userId, demStr, callback){
 	// demStr must be of type "Unknown", "Positive", "Negative"
 	fetch(url+"v1/send_dementia", {
@@ -143,6 +174,7 @@ export function backend_SetDementia(userId, demStr, callback){
 	.catch((err)=>{ console.log("BACKEND: "+err.message) } )
 }
 
+// not implemted crewAI yet, for now just returns .content
 export function backend_GetNews(callback){
 	fetch(url+"v1/get_news", {
 		method: 'POST',
--- src/pages/Patients.js
+++ src/pages/Patients.js
@@ -1,6 +1,15 @@
 import React from "react";
+import { backend_GetNews, backend_login } from "../backend.js";
 
 function Patients() {
+
+  // example usage (note danis code only sometimes responds to requests so until I fix that might need to call multiple times (will get 501 not implemented)
+  // note: i dont think calling functions directly in function body is a best practise in react but that sounds like a frontend person problem
+  backend_GetNews( e => console.log(e))
+  backend_GetNews( e => console.log(e))
+  backend_GetNews( e => console.log(e))
+  backend_GetNews("Eoin","12345", e => console.log(e)) // last argument is callback that recieves response, all prev arguments are part of request
+
   return (
     <div style={{ padding: "2rem" }}>
       <h1>Patient Dashboard</h1>
